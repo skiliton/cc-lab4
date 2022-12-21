fd4d1acecc13893da9e29f8db5e6aeb28f83e63c
ivanrepeta <skiliton221@gmail.com>
2022-12-22 00:14:48 +0200

    Inital commit

---
+++ src/main/kotlin/ua/repeta/cclab4/CcLab4Application.kt
@@ -0,0 +1,11 @@
+package ua.repeta.cclab4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CcLab4Application
+
+fun main(args: Array<String>) {
+	runApplication<CcLab4Application>(*args)
+}
---
+++ src/test/kotlin/ua/repeta/cclab4/CcLab4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.repeta.cclab4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CcLab4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
